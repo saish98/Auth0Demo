df7f02fde7b5f0d85c7a004cbf827aeb1e435228
saish98 <saish98@gmail.com>
2021-08-09 18:51:58 +0530

    initial commit

---
+++ Auth0Demo/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Auth0Demo
+//
+//  Created by Saish Chachad on 09/08/21.
+//
+
+import UIKit
+import Auth0
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textFieldEmail: UITextField!
+    @IBOutlet private weak var textFieldPassword: UITextField!
+    
+    fileprivate var retrievedCredentials: Credentials?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func buttonLoginAction(_ sender: Any) {
+        
+        Auth0
+            .authentication()
+            .login(
+                usernameOrEmail: self.textFieldEmail.text!,
+                password: self.textFieldPassword.text!,
+                realm: "Username-Password-Authentication",
+                scope: "openid profile")
+            .start { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let credentials):
+                        self.retrievedCredentials = credentials
+                        
+                    case .failure(let error):
+                        print("error: \(error.localizedDescription)")
+                    }
+                }
+        }
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Auth0Demo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Auth0Demo
+pod 'Auth0'
+
+end
