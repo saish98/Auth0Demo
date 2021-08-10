32f62dbecc86f5381173e1c8a5d8920c633b75a3
Shivam Kapur <shivam@heady.io>
2021-08-10 17:18:07 +0530

    login, token renew, credentials manager

--- Auth0Demo/ViewController.swift
+++ Auth0Demo/ViewController.swift
@@ -22,7 +22,12 @@ class ViewController: UIViewController {
 
 
     @IBAction func buttonLoginAction(_ sender: Any) {
+        let auth = Auth0
+            .authentication(clientId: "", domain: "")
         
+        // keychain
+        var credentialManager = CredentialsManager.init(authentication: auth)
+
         Auth0
             .authentication()
             .login(
@@ -35,11 +40,43 @@ class ViewController: UIViewController {
                     switch result {
                     case .success(let credentials):
                         self.retrievedCredentials = credentials
+                        credentialManager.store(credentials: credentials)
+                        credentialManager.enableBiometrics(withTitle: "For security", cancelTitle: "Not Now", fallbackTitle: "?", evaluationPolicy: .deviceOwnerAuthenticationWithBiometrics)
                         print("Success: \(credentials.description)")
                     case .failure(let error):
                         print("error: \(error.localizedDescription)")
                     }
                 }
+        }
+        
+        
+        // Logout
+        _ = credentialManager.clear()
+        
+    }
+    
+    private func retrieveCredentials() {
+        let auth = Auth0
+            .authentication(clientId: "", domain: "")
+        
+        // keychain
+        let credentialManager = CredentialsManager.init(authentication: auth)
+
+        // Check if session is valid
+        if credentialManager.hasValid() {
+            // if yes fetch credentials
+            credentialManager.credentials { error, credentials in
+                // renew token
+                let request = auth.renew(withRefreshToken: credentials?.refreshToken ?? "", scope: "openid profile offline_access").start { newCreds in
+                    // store new credentials
+                    credentialManager.store(credentials: try! newCreds.get())
+                }
+                
+            }
+        } else {
+            // logout 
+            credentialManager.clear()
+            // login again
         }
     }
 }
